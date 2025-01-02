70b8debf026ca756e00749006d027608f48fc5e3
Danu-Nur <diramadhan13@gmail.com>
2025-01-02 22:48:16 +0700

    Add Menu class with widget titles, descriptions, tab indices, and icons

---
+++ lib/common/menu.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Menu {
+  static const List<String> widgetTitles = [
+    'Bank',
+    'Wallet',
+    'User',
+    'Category',
+    'Label',
+    'Budget',
+    'Inventory',
+    'Assets',
+  ];
+  static const List widgetDesc = [
+    'List of bank',
+    'List of wallet',
+    'List of user',
+    'List of category',
+    'List of label',
+    'List of budget',
+    'List of inventory',
+    'List of assets',
+  ];
+  static const List widgetOnTabs = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+  ];
+  static const List widgetIconsMenu = [
+    Icons.account_balance,
+    Icons.account_balance_wallet,
+    Icons.group,
+    Icons.category,
+    Icons.label,
+    Icons.attach_money,
+    Icons.inventory_2,
+    Icons.business_center,
+  ];
+}
